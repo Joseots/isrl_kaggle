404e17dcf8781b1c769bd10fd579a4488649af30
Joseots <joseotoledo@gmail.com>
2023-05-11 17:39:00 -0400

    Changes to be committed: 	new file:   data_generator.py
    Changes not staged for commit:
    	modified:   .gitignore

---
+++ data_generator.py
@@ -0,0 +1,82 @@
+# Imports
+import tensorflow as tf
+import pandas as pd
+import json
+import numpy as np
+
+# Parquet Dataset
+DATAPATH = '/mnt/f/datasets/isrl_kaggle/'
+ROWS_PER_FRAME = 543  # number of landmarks per frame
+LANDMARKS = [61, 40, 37, 0, 267, 270, 291, 91, 84, 17, 314, 321, 78, 81, 13,
+             311, 308, 178, 14, 402] + np.arange(468, 512).tolist() + \
+             np.arange(522, 543).tolist()
+train_set = pd.read_csv(DATAPATH + 'train.csv')
+with open(DATAPATH + 'sign_to_prediction_index_map.json') as f:
+    index_map = json.load(f)
+
+
+# Data Generator
+class DataGenerator(tf.keras.utils.Sequence):
+    '''Generates data for Keras'''
+    def __init__(self, list_IDs, train_set, labels, batch_size=32,
+                 dim=(160, 85), n_channels=3, n_classes=250, shuffle=True):
+        '''Initialization'''
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.train_set = train_set
+        self.on_epoch_end()
+
+    def __len__(self):
+        '''Denotes the number of batches per epoch'''
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        '''Generate one batch of data'''
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        '''Updates indexes after each epoch'''
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle is True:
+            np.random.shuffle(self.indexes)
+
+    def load_relevant_data_subset(self, pq_path):
+        data_columns = ['x', 'y', 'z']
+        data = pd.read_parquet(pq_path, columns=data_columns)
+        n_frames = int(len(data) / ROWS_PER_FRAME)
+        data = data.values.reshape(n_frames, ROWS_PER_FRAME, len(data_columns))
+        return data.astype(np.float32)
+
+    def __data_generation(self, list_IDs_temp):
+        '''Generates data containing batch_size samples'''
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            current_path = DATAPATH + self.train_set.path[ID]
+            data = self.load_relevant_data_subset(current_path)
+            data = np.nan_to_num(data[:, LANDMARKS, :], 0)
+            data = tf.image.resize(data, size=self.dim, method='nearest')
+            X[i, ] = data.numpy()
+
+            # Store class
+            y[i] = self.labels[self.train_set.sign[ID]]
+
+        return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
